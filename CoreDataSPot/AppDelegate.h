16ab2c20521dd84f418508cbe0c43c44a520f254
tsunglintsai <tsunglin.tsai@gmail.com>
2013-03-07 16:43:23 -0800

    Initial Commit

---
+++ CoreDataSPot/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoreDataSPot
+//
+//  Created by Daniela on 3/7/13.
+//  Copyright (c) 2013 Pyrogusto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CoreDataSPot/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreDataSPot
+//
+//  Created by Daniela on 3/7/13.
+//  Copyright (c) 2013 Pyrogusto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
